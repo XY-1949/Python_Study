b1a49e680a0dfbc4d561e701323adf071ce0fc06
XY_1949 <y2629262@163.com>
2025-03-25 17:27:07 +0800

    更新：同步本地Python学习代码

---
+++ Day01/day01.py
@@ -0,0 +1,29 @@
+# 第一个测试文件
+print("hello world")
+'''
+使用import导入模块，类别java使用import调用包
+使用from import导入模块
+
+'''
+
+"""
+这个也是注释，就神奇
+"""
+import keyword
+import os
+
+keyword.kwlist
+print("输出的数据的内容为:")
+print(os.getcwd())
+print(keyword.kwlist.count)
+# 生成一个10以内的随机数
+import random
+num1 = random.randint(1, 10)
+if
+
+test = True
+
+if test:
+    print("这里是正确的内容")
+else:
+    print("这里是错误的内容")
